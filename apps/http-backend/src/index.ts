29982f79268a587e629ec79df289445168462bc6
Pavan Kushnure <2019bcs044@sggs.ac.in>
2025-03-30 16:17:50 +0530

    added router

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import authRouter from "./route/authRouter";
+import roomRouter from "./route/roomRouter";
 const app = express();
-
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
-app.use("/api/rooms",)
+app.use("/api/rooms", roomRouter);
 
 app.listen(3001);
